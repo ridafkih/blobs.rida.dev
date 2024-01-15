d689e8c8566d9d6d699bb95556efd8590385b674
Rida F'kih <hello@rida.dev>
2024-01-14 19:33:34 -0500

    Open endpoint for client uploads

--- src/app/artifacts/[[...segments]]/route.ts
+++ src/app/artifacts/[[...segments]]/route.ts
@@ -1,7 +1,47 @@
-import { put,  } from "@vercel/blob";
-import type { NextRequest } from "next/server";
+import { put } from "@vercel/blob";
+import { handleUpload } from "@vercel/blob/client";
+import { type NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
+
+export const POST = async (request: NextRequest): Promise<NextResponse> => {
+  const destination = request.nextUrl.pathname.split("/").slice(2).join("/");
+  console.log(destination)
+ 
+  try {
+    const jsonResponse = await handleUpload({
+      body: {
+        type: "blob.generate-client-token",
+        payload: {
+          pathname: destination,
+          callbackUrl: "",
+          multipart: false,
+          clientPayload: null,
+        }
+      },
+      request,
+      onBeforeGenerateToken: async (
+        pathname: string,
+      ) => { 
+        return {
+          tokenPayload: JSON.stringify({
+            pathname,
+            "x-id": request.headers.get("x-id"),
+          }),
+        };
+      },
+      onUploadCompleted: async ({ blob, tokenPayload }) => {},
+    });
+ 
+    if (!("clientToken" in jsonResponse)) return new NextResponse(null, { status: 400 });
+    return new NextResponse(jsonResponse.clientToken);
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 400 },
+    );
+  }
+}
 
 export const PUT = async (request: NextRequest) => {  
   const destination = request.nextUrl.pathname.slice(1);
